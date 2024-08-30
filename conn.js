4f1f7fd6240abe7d12d80c70703c99ccb1263b97
santhuuuu <153983969+santhuuuu@users.noreply.github.com>
2024-08-30 22:45:31 +0530

    Add files via upload

---
+++ conn.js
@@ -0,0 +1,12 @@
+
+const mongoose = require('mongoose')
+require('dotenv').config()
+const connectDb = () =>{
+    mongoose.connect(process.env.MONGO_URI).then(()=>{
+        console.log("Database connected")
+    }).catch((err)=>{
+        console.log(err)
+    })
+}
+
+module.exports = connectDb
